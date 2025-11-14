b0611e2a15c8067382b4cd6c08b6fd59d173072d
Samuel Ndegwa Kiarie <sammiekiarie90@gmail.com>
2025-11-14 21:02:59 +0300

    alot

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,11 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\BriefController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::middleware(['auth'])->group(function () {
-    Route::resource('briefs', BriefController::class);
-});
-
 
 Route::get('/dashboard', function () {
     return view('dashboard');
